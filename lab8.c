2cda355837092e820aea155881c83b883164913c
janushhs <janushhs9@gmail.com>
2025-10-27 09:43:28 +0530

    Add files via upload

---
+++ lab8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    printf("1: Operating System\n");
+    printf("2: DevOps\n");
+    printf("3: Statistics and Probability\n");
+    printf("4: Data Structure\n");
+    printf("5: Digital Circuits and Computer Organization\n");
+    printf("6: Social Connect\n");
+    return 0;
+}
+
+
+
